091c50fc837d48da4c9d5f4443059ea6cbd00195
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 14:16:50 +0700

    add generator

--- blocks/blocks_buzzer.js
+++ blocks/blocks_buzzer.js
@@ -1,15 +1,18 @@
-Blockly.Blocks['ky_012_buzzer_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-012 BUZZER");
-    this.appendValueInput("KY_012_BUZZER_PIN")
-        .setCheck("Number")
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_012_buzzer_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-012 BUZZER");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "KY_012_BUZZER_PIN")
+        .appendField("SET STATE")
+        .appendField(
+          new Blockly.FieldVariable("KY_012_BUZZER_STATE"),
+          "KY_012_BUZZER_STATE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_buzzer.js
+++ blocks/generators_buzzer.js
@@ -1,6 +1,20 @@
-Blockly.JavaScript['ky_012_buzzer_block'] = function(block) {
-  var value_ky_012_buzzer_pin = Blockly.JavaScript.valueToCode(block, 'KY_012_BUZZER_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_012_buzzer_block"] = function(block) {
+    var text_ky_012_buzzer_pin = block.getFieldValue("KY_012_BUZZER_PIN");
+    var variable_ky_012_buzzer_state = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_012_BUZZER_STATE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+    #VARIABLE
+	int KY_012_BUZZER_PIN = ${text_ky_012_buzzer_pin};
+	#END
+	
+	#SETUP
+	  pinMode (KY_012_BUZZER_PIN, OUTPUT);
+	#END
+
+	${variable_ky_012_buzzer_state} = KY_012_BUZZER_PIN;
+    `;
+    return code;
+  };
